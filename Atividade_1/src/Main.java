acc9951c1a42fd81b35b294dbfc7d20f96b1cc69
Sabrina <sabrinaslimap@gmail.com>
2024-03-06 22:02:11 -0300

    ajuste exercicio 3

--- Atividade_1/src/Main.java
+++ Atividade_1/src/Main.java
@@ -71,8 +71,11 @@ public class Main {
         System.out.println("----------------------------------------------------------------");
         System.out.println("-------------------------- Exercício 4 --------------------------");
 
-        System.out.println("Digite seu nome e sobrenome:");
-        String nomeCompleto = scanner.nextLine();
+        System.out.println("Digite seu nome:");
+        String nomeDigitado = scanner.nextLine();
+
+        System.out.println("Digite seu sobrenome:");
+        String sobrenome = scanner.nextLine();
 
         System.out.println("Digite sua idade: ");
         int idade = scanner.nextInt();
@@ -81,7 +84,7 @@ public class Main {
         System.out.println("Digite seu endereço: ");
         String endereco = scanner.nextLine();
 
-        System.out.println("Nome completo: " + nomeCompleto + " | Idade: " + idade + " | Endereço: " + endereco);
+        System.out.println("Nome completo: " + nomeDigitado + " " + sobrenome + " | Idade: " + idade + " | Endereço: " + endereco);
 
         /* ------------------------------------------------------------------------------------------------------
         5 - Crie um algoritmo que cadastra o nome e a idade de um usuario, após o cadastro mostre em que ano esse
